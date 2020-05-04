3ba62a4d0745ac5643670af4515cd1422e711b7b
Scott Wu <2974920+sacrosanctic@users.noreply.github.com>
2020-05-04 10:01:36 -0700

    .

---
+++ src/js/calculation.js
@@ -0,0 +1,135 @@
+//source: https://github.com/frankkarsten/MTG-Math/blob/master/NivMizzet.py
+/*
+Parameters:
+  n - Number of elements of the entire set
+  k - Number of elements in the subset
+It should hold that 0 <= k <= n
+Returns - The binomial coefficient n choose k that represents the number of ways of picking k unordered outcomes from n possibilities
+*/
+function binom(n,k) {
+  let answer=1
+  for(let i=1;i<Math.min(k,n-k)+1;i++)
+    answer=(answer*(n+1-i))/i;
+  return parseInt(answer);
+}
+/*
+Parameters:
+  deck - A dictionary of cardname : number of copies
+  needed - A dictionary of cardname : number of copies
+It should hold that the cardname keys of deck and needed are identical
+Returns - the multivariate hypergeometric probability of drawing exactly the cards in 'needed' from 'deck' when drawing without replacement
+*/
+export function multivariate_hypgeom(deck, needed) {
+  let answer=1
+  let sum_deck=0
+  let sum_needed=0
+  for(const card in deck) {
+    answer*=binom(deck[card],needed[card]);
+    sum_deck+=deck[card];
+    sum_needed+=needed[card];
+  }
+  return answer/binom(sum_deck,sum_needed);
+}
+/*
+Parameters:
+number_hits - Should be between 0 or 10. Represents the number of cards put into your hand with Niv.
+Returns - a number that represents the probability of hitting <number_hits> different guilds in your top 10.
+test case:
+4,2,1,3,2,0,2,0,1,3,42
+average 2.68, 1 in 210438
+1.8,12.5,30.2,32.8,17.4,4.6,0.6,0
+*/
+export function determine_hit_prob(deck, number_hits) {
+  let hit_prob = 0;
+  for (let WU = 0; WU < deck["WU"] + 1; WU++) {
+    for (let WB = 0; WB < deck["WB"] + 1; WB++) {
+      for (let WR = 0; WR < deck["WR"] + 1; WR++) {
+        for (let WG = 0; WG < deck["WG"] + 1; WG++) {
+          for (let UB = 0; UB < deck["UB"] + 1; UB++) {
+            for (let UR = 0; UR < deck["UR"] + 1; UR++) {
+              for (let UG = 0; UG < deck["UG"] + 1; UG++) {
+                for (let BR = 0; BR < deck["BR"] + 1; BR++) {
+                  for (let BG = 0; BG < deck["BG"] + 1; BG++) {
+                    for (let RG = 0; RG < deck["RG"] + 1; RG++) {
+
+  const cards_so_far=WU+WB+WR+WG+UB+UR+UG+BR+BG+RG
+  let hits_so_far =
+    Math.min(WU,1) +
+    Math.min(WB,1) +
+    Math.min(WR,1) +
+    Math.min(WG,1) +
+    Math.min(UB,1) +
+    Math.min(UR,1) +
+    Math.min(UG,1) +
+    Math.min(BR,1) +
+    Math.min(BG,1) +
+    Math.min(RG,1)
+  if(
+    cards_so_far<=10 &&
+    hits_so_far==number_hits
+  ) {
+    let needed={};
+    needed["WU"]=WU;
+    needed["WB"]=WB;
+    needed["WR"]=WR;
+    needed["WG"]=WG;
+    needed["UB"]=UB;
+    needed["UR"]=UR;
+    needed["UG"]=UG;
+    needed["BR"]=BR;
+    needed["BG"]=BG;
+    needed["RG"]=RG;
+    needed["Other"]=10-cards_so_far
+    hit_prob+=multivariate_hypgeom(deck,needed)
+  }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  return hit_prob;
+}
+
+// https://www.geeksforgeeks.org/find-all-combinations-that-adds-upto-given-number-2/
+/* Function to find out all combinations of positive numbers
+that add upto given number. Includes duplicates */
+let permutation = []
+export function findCombination(n) {
+  let arr = []
+  findCombinationsRecursion(arr,0,n,n)
+  return permutation
+}
+function findCombinationsRecursion(arr,index,num,reducedNum) {
+  // Base condition
+  if (reducedNum<0 || index>10) return
+
+  // If combination is found, print it
+  if (reducedNum==0) {
+    let temp = []
+    for (let i=0;i<index;i++) temp.push(arr[i])
+    permutation.push(temp)
+    return
+  }
+
+  // Find the previous number
+  // stored in arr[] It helps
+  // in maintaining increasing order
+  const prev = index==0?1:arr[index-1]
+
+  // note loop starts from previous
+  // number i.e. at array location
+  // index - 1
+  for (let k=prev;k<=num;k++) {
+    // next element of array is k
+    arr[index]=k
+
+    // call recursively with
+    // reduced number
+    findCombinationsRecursion(arr,index+1,num,reducedNum-k)
+  }
+}
--- src/views/About.vue
+++ src/views/About.vue
@@ -15,7 +15,7 @@
         <v-text-field value="amount" v-model="hitSpreadInput"></v-text-field>
       </v-col>
       <v-col>
-        <v-btn @click="spreadLookup(hitSpreadInput)">Lookup</v-btn>
+        <v-btn @click="getResult(hitSpreadInput)">Lookup</v-btn>
       </v-col>
     </v-row>
     <v-row>
@@ -40,11 +40,8 @@
 </template>
 
 <script>
-// [8:55 PM, 5/3/2020] R: import calcFunc from '@/path/to/files/calc'
-// [8:56 PM, 5/3/2020] R: Then create a property and assign it calcFunc
-// [8:56 PM, 5/3/2020] R: calcFunc: calcFunc
 import BarChart from "@/components/BarChart"
-import {binom} from "@/components/MyCalculator.js"
+import { determine_hit_prob, findCombination } from '@/js/calculation.js'
 
 export default {
   // mixins: [binom,simple],
@@ -187,21 +184,6 @@ export default {
   }),
   mounted() {
     this.setChartData([0])
-
-    // const obj = {
-    //   n: 10,
-    //   k: 10,
-    // }
-    // console.time("vuex binom")
-    // this.$store.dispatch('binom',obj)
-    // .then(res=>{
-    //   console.timeEnd("vuex binom")
-    //   console.log(res)
-    // })
-    // console.time('binom')
-    // console.log(this.binom(10,10))
-    // console.timeEnd('binom')
-
   },
   methods: {
     setChartData(data) {
@@ -217,9 +199,8 @@ export default {
         ]
       }
     },
-    spreadLookup(value) {
-      let array = value.split(',').map(Number)
-      this.calculateProbability(this.loadData(array))
+    getResult(data) {
+      this.calculateProbability(...this.formatData(data.split(',').map(Number)))
     },
     exportCSV() {
       let csvContent = "data:text/csv;charset=utf-8,"
@@ -239,85 +220,50 @@ export default {
     clear() {
       this.results = []
     },
+    //loop through one to x combination of hits
     combinationLoop2(numHit) {
-      //loop through one to x combination of hits
-      for (let i=1;i<=numHit;i++) {
+      for(let i=1;i<=numHit;i++)
         this.combinationLoop(i)
-      }
     },
+    // loop through x combination of hits
     combinationLoop(numHit) {
-      // loop through x combination of hits
-      this.combination = []
-      this.findCombinations(numHit)
-      for(const [i,item] of this.combination.entries()) {
+      let combination = []
+      combination = this.findCombination(numHit)
+      for(const [i,item] of combination.entries()) {
         item
-        // this.calculateProbability(this.loadData([...item,60-numHit]))
-        console.log('progress: '+this.combination.length + '-'+ this.possibleHits + '-' + (i+1))
-        this.log = 'progress: '+this.combination.length + '-'+ this.possibleHits + '-' + (i+1)
+        this.calculateProbability(...this.formatData([...item,60-numHit]))
+        console.log('progress: '+combination.length + '-'+ this.possibleHits + '-' + (i+1))
+        this.log = 'progress: '+combination.length + '-'+ this.possibleHits + '-' + (i+1)
       }
     },
-    loadData(array) {
-      this.deck.Other = array.pop() - 1 //remaining cards minus the first niv-mizzet
-      this.deck.WU = (array[0]||0)
-      this.deck.WR = (array[2]||0)
-      this.deck.WB = (array[1]||0)
-      this.deck.WG = (array[3]||0)
-      this.deck.UB = (array[4]||0)
-      this.deck.UR = (array[5]||0)
-      this.deck.UG = (array[6]||0)
-      this.deck.BR = (array[7]||0)
-      this.deck.BG = (array[8]||0)
-      this.deck.RG = (array[9]||0)
+    formatData(arr) {
+      // this.deck.Other = arr.pop()-1 //remaining cards minus the first niv-mizzet
+      // this.deck.WU = (arr[0]||0)
+      // this.deck.WR = (arr[2]||0)
+      // this.deck.WB = (arr[1]||0)
+      // this.deck.WG = (arr[3]||0)
+      // this.deck.UB = (arr[4]||0)
+      // this.deck.UR = (arr[5]||0)
+      // this.deck.UG = (arr[6]||0)
+      // this.deck.BR = (arr[7]||0)
+      // this.deck.BG = (arr[8]||0)
+      // this.deck.RG = (arr[9]||0)
+      // console.log(this.deck)
+      // return this.deck
 
-      this.possibleHits = array.reduce((a,b)=>a+b,0)
-      // this.deck.Total = numHit
-      return this.deck
+      const guilds = ["WU", "WR", "WB", "WG", "UB", "UR", "UG", "BR", "BG", "RG"]
+      return [
+        guilds.reduce(
+          (o,a,i)=>({...o,[a]:arr[i]}),
+          {other:arr.pop()-1}
+        ),
+        arr.reduce((a,b)=>a+b,0)
+      ]
     },
-    // https://www.geeksforgeeks.org/find-all-combinations-that-adds-upto-given-number-2/
-    /* Function to find out all
-      combinations of positive numbers
-      that add upto given number.
-      Includes duplicates */
-    findCombinations($n) {
-      // array to store the combinations
-      // It can contain max n elements
-      let $arr = []
-      //find all combinations
-      this.findCombinationsRecursion($arr, 0, $n, $n)
-    },
-    findCombinationsRecursion($arr, $index, $num, $reducedNum) {
-      // Base condition
-      if ($reducedNum < 0 || $index > 10) return
-
-      // If combination is
-      // found, print it
-      if ($reducedNum == 0) {
-        let temp = []
-        for (let $i = 0; $i < $index; $i++) temp.push($arr[$i])
-        this.combination.push(temp)
-        return
-      }
-
-      // Find the previous number
-      // stored in arr[] It helps
-      // in maintaining increasing order
-      const $prev = $index == 0 ? 1 : $arr[$index - 1]
-
-      // note loop starts from previous
-      // number i.e. at array location
-      // index - 1
-      for (let $k = $prev; $k <= $num; $k++) {
-        // next element of array is k
-        $arr[$index] = $k
-
-        // call recursively with
-        // reduced number
-        this.findCombinationsRecursion($arr, $index + 1, $num, $reducedNum - $k)
-      }
-    },
-    calculateProbability(deck) {
-      let obj = {}
+    calculateProbability(deck,possibleHits) {
       let expected_hits = 0
+      let obj = {}
+
       for (let number_hits = 0; number_hits <= 10; number_hits++) {
         const hit_prob = this.determine_hit_prob(deck,number_hits)
         obj[number_hits]=(hit_prob*100).toFixed(2)
@@ -327,110 +273,16 @@ export default {
 
       let maxGuild = 0;
       for (const [key, value] of Object.entries(deck)) {
-        if (key == "Other") continue;
+        if (key == 'Other') continue;
         if (value != 0) maxGuild++;
       }
       obj.maxHit=maxGuild
       obj.maxHitChance="1 in "+(1/this.determine_hit_prob(deck,maxGuild)).toFixed(0)
-      this.results.push({...deck,...obj,possibleHits: this.possibleHits,deckSize:this.possibleHits+ deck.Other+1})
+      this.results.push({...deck,...obj,possibleHits:possibleHits,deckSize:possibleHits+ deck.Other+1})
       this.setChartData(Object.values(obj).slice(0,10))
     },
-    //source: https://github.com/frankkarsten/MTG-Math/blob/master/NivMizzet.py
-    // binom(n, k) {
-    //   /*
-    //   Parameters:
-    //     n - Number of elements of the entire set
-    //     k - Number of elements in the subset
-    //   It should hold that 0 <= k <= n
-    //   Returns - The binomial coefficient n choose k that represents the number of ways of picking k unordered outcomes from n possibilities
-    //   */
-    //   let answer = 1;
-    //   for (let i = 1; i < Math.min(k, n - k) + 1; i++)
-    //     answer = (answer * (n + 1 - i)) / i;
-    //   return parseInt(answer);
-    // },
-    multivariate_hypgeom(deck, needed) {
-      /*
-      Parameters:
-        deck - A dictionary of cardname : number of copies
-        needed - A dictionary of cardname : number of copies
-      It should hold that the cardname keys of deck and needed are identical
-      Returns - the multivariate hypergeometric probability of drawing exactly the cards in 'needed' from 'deck' when drawing without replacement
-      */
-      let answer = 1;
-      let sum_deck = 0;
-      let sum_needed = 0;
-      for (const card in deck) {
-        //needs work
-        answer *= binom(deck[card], needed[card]);
-        sum_deck += deck[card];
-        sum_needed += needed[card];
-      }
-      return answer / binom(sum_deck, sum_needed);
-    },
-    determine_hit_prob(deck, number_hits) {
-      /*
-      Parameters:
-      number_hits - Should be between 0 or 10. Represents the number of cards put into your hand with Niv.
-      Returns - a number that represents the probability of hitting <number_hits> different guilds in your top 10.
-      */
-      let hit_prob = 0;
-      for (let WU = 0; WU < deck["WU"] + 1; WU++) {
-        for (let WB = 0; WB < deck["WB"] + 1; WB++) {
-          for (let WR = 0; WR < deck["WR"] + 1; WR++) {
-            for (let WG = 0; WG < deck["WG"] + 1; WG++) {
-              for (let UB = 0; UB < deck["UB"] + 1; UB++) {
-                for (let UR = 0; UR < deck["UR"] + 1; UR++) {
-                  for (let UG = 0; UG < deck["UG"] + 1; UG++) {
-                    for (let BR = 0; BR < deck["BR"] + 1; BR++) {
-                      for (let BG = 0; BG < deck["BG"] + 1; BG++) {
-                        for (let RG = 0; RG < deck["RG"] + 1; RG++) {
-                          const cards_so_far =
-                            WU + WB + WR + WG + UB + UR + UG + BR + BG + RG
-                          let hits_so_far =
-                            Math.min(WU, 1) +
-                            Math.min(WB, 1) +
-                            Math.min(WR, 1) +
-                            Math.min(WG, 1) +
-                            Math.min(UB, 1) +
-                            Math.min(UR, 1) +
-                            Math.min(UG, 1) +
-                            Math.min(BR, 1) +
-                            Math.min(BG, 1) +
-                            Math.min(RG, 1)
-                          if (
-                            cards_so_far <= 10 &&
-                            hits_so_far == number_hits
-                          ) {
-                            let needed = {};
-                            needed["WU"] = WU;
-                            needed["WB"] = WB;
-                            needed["WR"] = WR;
-                            needed["WG"] = WG;
-                            needed["UB"] = UB;
-                            needed["UR"] = UR;
-                            needed["UG"] = UG;
-                            needed["BR"] = BR;
-                            needed["BG"] = BG;
-                            needed["RG"] = RG;
-                            needed["Other"] = 10 - cards_so_far
-                            hit_prob += this.multivariate_hypgeom(
-                              deck,
-                              needed
-                            )
-                          }
-                        }
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-      return hit_prob;
-    }
+    determine_hit_prob: determine_hit_prob,
+    findCombination: findCombination,
   }
 }
 </script>
